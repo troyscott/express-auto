e30d6bb5004c7da80ba3556199846bef5587892d
Troy Scott <troys2005@gmail.com>
2012-06-02 21:07:22 -0700

    implemnted authentication for the ContentProvider.  logic should be separated from the contentprovider module for reuse

--- models/content-provider.js
+++ models/content-provider.js
@@ -1,30 +1,64 @@
-var Db = require('mongodb').Db;
-var Connection = require('mongodb').Connection;
-var Server = require('mongodb').Server;
-var BSON = require('mongodb').BSON;
-var ObjectID = require('mongodb').ObjectID;
+var server_options = {auto_reconnect:true},
+    username = '',
+    password = '',
+    server = '',
+    port = 0
+
+var mongodb = require('mongodb'),
+    mongoserver = new mongodb.Server(server, port, server_options ),
+    db_connection = new mongodb.Db('tscdev', mongoserver);
+
+function openDB(callback) {
+
+    db_connection.open(function(error, db) {
+        if(error) callback(error);
+        console.log('Database connected');
+        callback(null, db)
+
+    });
+
+}
 
 
+function authenticate(db,username, password, type, callback) {
 
-ContentProvider = function(host, port){
+    console.log('Attempt authentication ');
+    db.authenticate( username, password, function(error, result) {
+        if (error) callback(error);
+
+        if (result) {
+            var collection = new mongodb.Collection(db, type);
+            callback(null, collection);
+
+        } else {callback(error) }
+
+    })
+      
+
+}
+
+// ContentProvider Class
+
+ContentProvider = function(){
     console.log('Starting Content Provider ...');
-    this.db= new Db('content-brighouse', new Server(host, port, {auto_reconnect: true}, {}));
-    this.db.open(function(){});
-
+    this.db = openDB(function(){});
+    
     console.log('ContentProvider Constructor');
     // Use the test route 
+    
     this.test= {data: [{code: "123", name: "12 Bolt Battery"}, {code:"456", name: "6 Volt Battery"} ]};    
 };
 
 
 ContentProvider.prototype.getList = function(type, callback) {
     console.log("ContentProvider List " + type + "'s");
-    this.db.collection(type, function(error, items) {
-            if(error)  callback(error);
-            else callback(null, items);
-            
-            });
+    
+    authenticate(db_connection, username, password, type, function(error, items) {
+        if(error) callback(error);
+        else callback(null, items);
 
+    });
+            
 }
 
 
